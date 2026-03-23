7b803d632fd70ddc5725d85f1f1b752e03ad962f
lucasnevespereira <pereiraneveslucas@gmail.com>
2026-03-23 23:26:04 +0100

    chore: list ports cmd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::process::Command;
+
 fn main() {
-    print!("Hello from {app} \n", app = "onport");
+    let list_ports = Command::new("lsof")
+        .args(["-i", "-P", "-n"])
+        .output()
+        .expect("failed to get ports");
+
+    match list_ports.status.success() {
+        true => {
+            print!("{}", String::from_utf8_lossy(&list_ports.stdout));
+        }
+        false => {
+            eprintln!(
+                "failed to list ports: {}",
+                String::from_utf8_lossy(&list_ports.stderr)
+            )
+        }
+    }
 }
